476dbf984076531e9961396a90d805a55bbd1a2d
Carolus Nathanell <c.nathanell05@gmail.com>
2026-03-10 11:31:49 +0700

    Classroom Code

--- trying_flutter_app/lib/main.dart
+++ trying_flutter_app/lib/main.dart
@@ -12,116 +12,119 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('my first app'),
-          centerTitle: true,
-          backgroundColor: Colors.red,
-        ),
-        body: Center(
-          child: Text('Hello World!',
-            style: TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
-              color: Colors.blueAccent,
-              letterSpacing: 3,
-              fontFamily: 'Dream'
-              ),
-            )
-          ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: (){},
-            child: Text('Button'),
-            backgroundColor: Colors.amber),
-          ),
       theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const RowColumnPage(),
+    );
+  }
+}
+
+class RowColumnPage extends StatelessWidget {
+  const RowColumnPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    MediaQueryData mediaQueryData = MediaQuery.of(context);
+    double screenWidth = mediaQueryData.size.width;
+    double screenHeight = mediaQueryData.size.height;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'My First App',
+          style: TextStyle(color: Colors.black),
+        ),
+        backgroundColor: Colors.orange[200],
+        centerTitle: true,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            child: AspectRatio(
+              aspectRatio: 1.0,
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                margin: EdgeInsets.fromLTRB(20.0, 5.0, 20.0, 10.0),
+                padding: EdgeInsets.all(20.0),
+                color: Colors.lightBlue[100],
+                child: Center(
+                  child: Image.network(
+                    'https://picsum.photos/200',
+                    fit: BoxFit.cover,
+                    width: 500,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            margin: EdgeInsets.fromLTRB(20.0, 5.0, 20.0, 10.0),
+            padding: EdgeInsets.all(20.0),
+            color: Colors.pink[200],
+            child: Text('What image is that', style: TextStyle(fontSize: 16)),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            color: Colors.yellow[200],
+            padding: EdgeInsets.all(20.0),
+            margin: EdgeInsets.fromLTRB(20.0, 5.0, 20.0, 5.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Column(children: [Icon(Icons.food_bank), Text("Food")]),
+                Column(children: [Icon(Icons.landscape), Text("Scenery")]),
+                Column(children: [Icon(Icons.people), Text("People")]),
+              ],
+            ),
+          ),
+          CounterCard(),
+        ],
       ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class CounterCard extends StatefulWidget {
+  const CounterCard({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<CounterCard> createState() => _CounterCardState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _CounterCardState extends State<CounterCard> {
+  int _counter = 0; // This is the state (data) that changes.
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _counter++; // Update the state.
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+    return Container(
+      margin: EdgeInsets.fromLTRB(20.0, 5.0, 20.0, 5.0),
+      padding: EdgeInsets.all(20.0),
+      width: MediaQuery.of(context).size.width,
+      color: Colors.cyan[100],
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text("Counter here: $_counter", style: TextStyle(fontSize: 16)),
+          Container(
+            color: Colors.cyan[200],
+            padding: EdgeInsets.all(5.0),
+            child: IconButton(
+              onPressed: _incrementCounter,
+              icon: Icon(Icons.add, color: Colors.black, size: 16),
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+          ),
+        ],
       ),
     );
   }
