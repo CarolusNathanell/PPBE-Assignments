b4ad8316abc881b89f7ef675a50d91613114af57
Carolus Nathanell <c.nathanell05@gmail.com>
2026-03-24 11:57:31 +0700

    Assignment 3

---
+++ trying_flutter_app/lib/models/task.dart
@@ -0,0 +1,13 @@
+// models/task.dart
+import 'package:isar/isar.dart';
+
+// this line is needed to generate file
+// then run dart run build_runner build
+part 'task.g.dart';
+
+@Collection()
+class Task {
+  Id id = Isar.autoIncrement;
+  late String text;
+  late String date;
+}
