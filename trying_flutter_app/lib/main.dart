77808aef0e954c4e2213c6fbb1c77682b4dcaf24
Carolus Nathanell <c.nathanell05@gmail.com>
2026-04-07 10:59:29 +0700

    Final Firebase CRUD

--- trying_flutter_app/lib/main.dart
+++ trying_flutter_app/lib/main.dart
@@ -1,30 +1,23 @@
-import 'package:trying_flutter_app/models/task_database.dart';
-import 'package:trying_flutter_app/pages/tasks_page.dart';
-import 'package:provider/provider.dart';
+import 'package:trying_flutter_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:trying_flutter_app/homepage.dart';
 
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await TaskDatabase.initialize();
-
-  runApp(
-    ChangeNotifierProvider(
-      create: (context) => TaskDatabase(),
-      child: const MyApp(),
-    )
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
   );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: TasksPage()
+      home: HomePage(),
     );
   }
 }
