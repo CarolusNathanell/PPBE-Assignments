89652053a503c1100d255a44ad345ce82a607c80
Carolus Nathanell <c.nathanell05@gmail.com>
2026-04-07 11:22:39 +0700

    Trying Firebase Auth

--- trying_flutter_app/lib/main.dart
+++ trying_flutter_app/lib/main.dart
@@ -1,23 +1,27 @@
-import 'package:trying_flutter_app/firebase_options.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:trying_flutter_app/screens/home.dart';
+import 'package:trying_flutter_app/screens/login.dart';
+import 'package:trying_flutter_app/screens/register.dart';
 import 'package:flutter/material.dart';
-import 'package:trying_flutter_app/homepage.dart';
-
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
-    );
+    return MaterialApp(initialRoute: 'login', routes: {
+      'home': (context) => const HomeScreen(),
+      'login': (context) => const LoginScreen(),
+      'register': (context) => const RegisterScreen(),
+    });
   }
 }
